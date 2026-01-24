2e8b186c3fcedd8f7c09e05533a9c8bbde14c2bf
matteo <48250758+matte-a@users.noreply.github.com>
2026-01-24 11:16:55 +0100

    up

--- romm/run.sh
+++ romm/run.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/with-contenv bash
 set -e
 
+# Export environment variables for ROMM
+export ROMM_AUTH_SECRET_KEY="${OPTIONS_romm_auth_secret_key}"
+export DATABASE_URL="${OPTIONS_database_url}"
+
+# Start ROMM
 exec /entrypoint.sh
