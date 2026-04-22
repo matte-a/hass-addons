11da066ca17e13305d74d299ee03b7169d86d37c
matteo <48250758+matte-a@users.noreply.github.com>
2026-04-22 20:52:35 +0200

    up

--- romm/run.sh
+++ romm/run.sh
@@ -6,4 +6,4 @@ export DATABASE_URL=$(grep -o '"database_url":"[^"]*"' /data/options.json | cut 
 export ROMM_DATA_DIR="/config"
 export ROMM_ROMS_DIR="/share/roms"
 
-exec python3 /app/main.py
+exec /docker-entrypoint.sh /init
