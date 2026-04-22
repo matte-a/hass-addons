d08c7c72ea06322cbba4dd353f1a4f704d5c31db
matteo <48250758+matte-a@users.noreply.github.com>
2026-04-22 21:01:44 +0200

    up

--- romm/run.sh
+++ romm/run.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
-set -e
 
-CONFIG=/data/options.json
+echo "=== OPTIONS ==="
+cat /data/options.json
 
-export ROMM_AUTH_SECRET_KEY=$(cat "$CONFIG" | python3 -c "import sys,json; print(json.load(sys.stdin)['romm_auth_secret_key'])")
-export DB_ENGINE="sqlite"
-export ROMM_DB_FILE="/config/romm/romm.db"
-export ROMM_DATA_DIR="/config/romm"
-export ROMM_LIBRARY_DIR="/share/roms"
+echo "=== ENV ==="
+env | grep -i romm
 
 mkdir -p /romm/config /romm/resources /romm/assets /redis-data /config/romm /share/roms
 touch /config/romm/romm.db
+
+export DB_ENGINE="sqlite"
+export ROMM_DB_FILE="/config/romm/romm.db"
+export ROMM_DB="sqlite:////config/romm/romm.db"
+export ROMM_DATA_DIR="/config/romm"
+export ROMM_LIBRARY_DIR="/share/roms"
 
 exec /docker-entrypoint.sh /init
