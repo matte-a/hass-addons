a432b8f9eb0136e14467c2ea8a034516bd972477
matteo <48250758+matte-a@users.noreply.github.com>
2026-04-22 21:15:46 +0200

    up

--- romm/run.sh
+++ romm/run.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 set -e
 
-export ROMM_AUTH_SECRET_KEY="supersecretkey123changeme"
+export ROMM_AUTH_SECRET_KEY=$(python3 -c "import json; d=json.load(open('/data/options.json')); print(d['romm_auth_secret_key'])")
+export DB_PASSWD=$(python3 -c "import json; d=json.load(open('/data/options.json')); print(d['db_passwd'])")
 export DB_HOST="core-mariadb"
 export DB_PORT="3306"
 export DB_NAME="romm"
 export DB_USER="romm"
-export DB_PASSWD="rommpassword"
 export ROMM_DATA_DIR="/config/romm"
 export ROMM_LIBRARY_DIR="/share/roms"
 
 mkdir -p /romm/config /romm/resources /romm/assets /redis-data /config/romm /share/roms
+touch /romm/config/config.yml
 
 exec /docker-entrypoint.sh /init
