5045773bf92429a5ab35e9d91b1fc6f9fcf818bb
matteo <48250758+matte-a@users.noreply.github.com>
2026-04-22 21:08:49 +0200

    up

--- romm/run.sh
+++ romm/run.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 set -e
 
-mkdir -p /romm/config /romm/resources /romm/assets /redis-data /config/romm /share/roms
-touch /config/romm/romm.db
-
 export ROMM_AUTH_SECRET_KEY="supersecretkey123changeme"
-export DB_ENGINE="sqlite"
-export ROMM_DB_FILE="/config/romm/romm.db"
+export DB_HOST="core-mariadb"
+export DB_PORT="3306"
+export DB_NAME="romm"
+export DB_USER="romm"
+export DB_PASSWD="rommpassword"
 export ROMM_DATA_DIR="/config/romm"
 export ROMM_LIBRARY_DIR="/share/roms"
+
+mkdir -p /romm/config /romm/resources /romm/assets /redis-data /config/romm /share/roms
 
 exec /docker-entrypoint.sh /init
